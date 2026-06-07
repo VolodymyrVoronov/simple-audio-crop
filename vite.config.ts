99fdc0e4405d15c345ccbeb664f695e1a89711c8
Volodymyr Voronov <malones.vladimir@gmail.com>
2026-06-07 13:24:05 +0200

    feat: add PWA support with vite-plugin-pwa and include app icon
    - Updated package.json to include vite-plugin-pwa dependency.
    - Added PWA icon image to public directory.
    - Configured Vite to support PWA with manifest and Workbox settings.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,36 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Simple Audio Crop",
+        short_name: "Simple Audio Crop",
+        description: "Crop audio files with a simple UI",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "./",
+        scope: "./",
+        icons: [
+          {
+            src: "./pwa-icon.png",
+            sizes: "24x24",
+            type: "image/png",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,mp3,wav}"],
+      },
+
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
